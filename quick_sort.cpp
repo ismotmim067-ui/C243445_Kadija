160f5e178eca9dd658b288010c873b41c100724a
Khadija Akter <ismotmim067@gmail.com>
2026-03-27 21:36:06 +0600

    Add files via upload

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,1 +1,58 @@
+#include <iostream>
+using namespace std;
 
+// Function to rearrange elements
+int divide(int arr[], int start, int end) {
+    int last = arr[end];   // taking last element
+    int i = start - 1;
+
+    for(int j = start; j < end; j++) {
+        if(arr[j] < last) {
+            i++;
+            // swap
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    // place last element in correct position
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[end];
+    arr[end] = temp;
+
+    return i + 1;
+}
+
+// Quick sort function
+void quickSort(int arr[], int start, int end) {
+    if(start < end) {
+        int position = divide(arr, start, end);
+
+        quickSort(arr, start, position - 1);
+        quickSort(arr, position + 1, end);
+    }
+}
+
+int main() {
+    int n;
+    cout<<"C243445"<<endl;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
